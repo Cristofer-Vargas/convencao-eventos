df0d65282b220ffecb3603cb2cfd7c61c13b8bad
Cristofer-Vargas <vargascristofer97@gmail.com>
2023-07-09 18:49:26 -0300

    feat(laravel): iniciando projeto com Laravel zerado, apenas com index modificado

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">Convenção de eventos Laravel</h1>
+
+Repositório de estudos relacionado o curso de laravel do canal Hora de Codar
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bem vindo!</title>
+</head>
+
+<body>
+	<h1>Início da aplicação</h1>
+</body>
+
+</html>
